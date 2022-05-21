5f0eb0e4d1f2697c46bc52014914fb759d616aac
i-colours-u <i.colours.u@gmail.com>
2022-05-22 03:51:31 +0900

    [feat] 카카오톡 공유하기 기능 완성

--- 30-SOPKATHON-iOS/30-SOPKATHON-iOS/Application/AppDelegate.swift
+++ 30-SOPKATHON-iOS/30-SOPKATHON-iOS/Application/AppDelegate.swift
@@ -6,7 +6,9 @@
 //
 
 import UIKit
-
+import KakaoSDKCommon
+import AuthenticationServices
+import FirebaseCore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    KakaoSDK.initSDK(appKey: "df520deba24c90ddb760281e934ec094")
     return true
   }
 
--- 30-SOPKATHON-iOS/30-SOPKATHON-iOS/Presentation/SplashScene/VC/SplashVC.swift
+++ 30-SOPKATHON-iOS/30-SOPKATHON-iOS/Presentation/SplashScene/VC/SplashVC.swift
@@ -6,22 +6,30 @@
 //
 
 import UIKit
+import KakaoSDKCommon
+import KakaoSDKTemplate
+import KakaoSDKLink
 
 class SplashVC: UIViewController {
-
+  
   // MARK: - UI Component Part
-
+  
   @IBOutlet weak var splashIconView: UIImageView!
   
   // MARK: - Life Cycle Part
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      delayWithSeconds(1) {
-        self.showAnimation()
-      }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.shareAction()
+
+    delayWithSeconds(1) {
+      self.showAnimation()
     }
-    
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+  }
+  
   private func showAnimation() {
     
     UIView.animate(withDuration: 1, delay: 0) {
@@ -37,7 +45,64 @@ class SplashVC: UIViewController {
     self.navigationController?.pushViewController(baseVC, animated: false)
   }
   
+  func shareAction() // 친구에게 공유하기
+  {
+  
+  
+  
+  
+  let messageJsonData =
+          """
+          {
+              "object_type": "feed",
+              "content": {
+                  "title": "국밥마라",
+                  "description": "오늘도 마라탕에 빠지고 마라탕...",
+          
+                  "image_url": "https://picsum.photos/300/300",
+                  "link": {
+                      "mobile_web_url": "https://developers.kakao.com",
+                      "web_url": "https://developers.kakao.com"
+                  }
+              },
+          
+              "buttons": [
+                  {
+                      "title": "나의 마라 혈중농도 확인하러 가기",
+                      "link": {
+                          "mobile_web_url": "https://developers.kakao.com",
+                          "web_url": "https://developers.kakao.com"
+                      }
+                  }
+              ]
+          }
+          """.data(using: .utf8)!
+  
+  
+  
+  
+  if let templatable = try? SdkJSONDecoder.custom.decode(FeedTemplate.self, from: messageJsonData) {
+    LinkApi.shared.defaultLink(templatable: templatable) {(linkResult, error) in
+      if let error = error {
+        print(error)
+        print("여기서 안될걸?2")
+      }
+      else {
+        print("defaultLink() success.")
+        
+        if let linkResult = linkResult {
+          UIApplication.shared.open(linkResult.url, options: [:], completionHandler: nil)
+        }
+      }
+    }
+    
+    
+  }
+  
+  
+  }
+  
   // MARK: - @objc Function Part
-
+  
 }
 // MARK: - Extension Part
