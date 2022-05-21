8c86fd2db5905a41d7c4085f1658f413fa9646b4
juhyeon <wngus4296@swu.ac.kr>
2022-05-22 01:49:57 +0900

    [ADD] FeedTVC 생성

---
+++ 30-SOPKATHON-iOS/30-SOPKATHON-iOS/Presentation/FeedScene/Cell/TVC/FeedTVC.swift
@@ -0,0 +1,20 @@
+//
+//  FeedTVC.swift
+//  30-SOPKATHON-iOS
+//
+//  Created by Juhyeon Byun on 2022/05/22.
+//
+
+import UIKit
+
+class FeedTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
