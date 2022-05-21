0114ea82a92dde4fa56569a6d82ed93546513758
i-colours-u <i.colours.u@gmail.com>
2022-05-22 08:25:46 +0900

    [chore] 데이터 모델 수정

--- 30-SOPKATHON-iOS/30-SOPKATHON-iOS/Data/Entity/Feed/FeedDataModel.swift
+++ 30-SOPKATHON-iOS/30-SOPKATHON-iOS/Data/Entity/Feed/FeedDataModel.swift
@@ -19,10 +19,10 @@ struct FeedDataModel {
 extension FeedDataModel {
     static let sampleData: [FeedDataModel] = [
         FeedDataModel(gaugeImageName: "angry_icon_4", gauge: 3, reason: "늦은 밤에 시끄러운 자식 너무 짜증나잖아!!!! \n화가난다!!!!!!!!!!!!!!"),
-        FeedDataModel(gaugeImageName: "angry_icon_4", gauge: 3, reason: "나를 빡치게한 이유 어쩌구"),
-        FeedDataModel(gaugeImageName: "angry_icon_4", gauge: 3, reason: "나를 빡치게한 이유 어쩌구"),
-        FeedDataModel(gaugeImageName: "angry_icon_4", gauge: 3, reason: "나를 빡치게한 이유 어쩌구"),
-        FeedDataModel(gaugeImageName: "angry_icon_4", gauge: 3, reason: "나를 빡치게한 이유 어쩌구"),
-        FeedDataModel(gaugeImageName: "angry_icon_4", gauge: 3, reason: "나를 빡치게한 이유 어쩌구")
+        FeedDataModel(gaugeImageName: "angry_icon_4", gauge: 3, reason: "야식 먹어서 폭주했어요.."),
+        FeedDataModel(gaugeImageName: "angry_icon_4", gauge: 2, reason: "지하철 아침에 놓침"),
+        FeedDataModel(gaugeImageName: "angry_icon_4", gauge: 3, reason: "버스 아침에 놓쳤어요..."),
+        FeedDataModel(gaugeImageName: "angry_icon_4", gauge: 3, reason: "떡볶이를 엎었어요.."),
+        FeedDataModel(gaugeImageName: "angry_icon_4", gauge: 3, reason: "잠이 와요..")
     ]
 }
